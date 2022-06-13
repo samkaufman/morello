0930b7afaa209d892bea52bd755d1ef41b58a65d
Sam Kaufman <emrysk@gmail.com>
2022-06-13 12:07:38 -0700

    Added schedule_search to top-level exports.

--- morello/__init__.py
+++ morello/__init__.py
@@ -1,7 +1,8 @@
-from . import dtypes, impl, specs, op_pprint, system_config
+from . import dtypes, impl, op_pprint, specs, system_config
 from .impl import spec_to_hole
+from .op_pprint import pformat, pprint
+from .search import schedule_search
 from .system_config import current_system, current_target, set_current_target
-from .op_pprint import pprint, pformat
 
 __all__ = [
     "current_system",
@@ -11,6 +12,7 @@ __all__ = [
     "op_pprint",
     "pformat",
     "pprint",
+    "schedule_search",
     "set_current_target",
     "spec_to_hole",
     "specs",
