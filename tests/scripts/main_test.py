54210345e2118cf0fb26dd6bf2a04979b9ab57c3
Sam Kaufman <emrysk@gmail.com>
2022-11-22 20:53:42 -0800

    Added main_test.py.

---
+++ tests/scripts/main_test.py
@@ -0,0 +1,10 @@
+import pathlib
+import subprocess
+import sys
+
+
+def test_main_script_can_be_run(request):
+    rootdir: pathlib.Path = request.config.rootdir
+    main_path = rootdir / "scripts" / "main.py"
+    script_args = ["--target", "cpu", "--serial", "matmul", "2", "2", "2"]
+    subprocess.run([sys.executable, str(main_path)] + script_args, check=True)
