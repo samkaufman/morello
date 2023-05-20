a9027f210c63bcbeafa856af2f60bb6626bc2983
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-05-20 04:42:43 -0700

    Use `-fopenmp`

--- morello/system_config/cpu.py
+++ morello/system_config/cpu.py
@@ -241,7 +241,7 @@ class X86Target(_CpuTarget):
 
 class ArmTarget(_CpuTarget):
     def _clang_vec_flags(self) -> Sequence[str]:
-        return []
+        return ["-fopenmp"]
 
 
 def _parse_benchmark_output(output: str) -> float:
