70c417b37dbcb79eeb57d184618a4ea97a6a29bc
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-08-09 12:48:42 -0700

    BuildArtifact -> BuiltArtifact

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -41,7 +41,7 @@ pub trait CodeGen<Tgt: Target> {
 
     fn emit<W: fmt::Write>(&self, out: &mut W) -> fmt::Result;
 
-    fn build(&self, print_code: bool) -> Result<BuildArtifact> {
+    fn build(&self, print_code: bool) -> Result<BuiltArtifact> {
         let dirname = tempdir()?.into_path();
         let source_path = dirname.join("main.c");
         let binary_path = dirname.join("a.out");
@@ -75,7 +75,7 @@ pub trait CodeGen<Tgt: Target> {
             println!("{}", String::from_utf8_lossy(&clang_proc.stderr));
         }
 
-        Ok(BuildArtifact::new(binary_path, source_path, dirname, None))
+        Ok(BuiltArtifact::new(binary_path, source_path, dirname, None))
     }
 }
 
@@ -93,14 +93,14 @@ impl<Tgt: Target<Level = X86MemoryLevel>, Aux: Clone + Debug> CodeGen<Tgt> for I
     }
 }
 
-pub struct BuildArtifact {
+pub struct BuiltArtifact {
     binary_path: PathBuf,
     source_path: PathBuf,
     whole_dir: PathBuf,
     benchmark_samples: Option<i32>,
 }
 
-impl BuildArtifact {
+impl BuiltArtifact {
     pub fn new(
         binary_path: PathBuf,
         source_path: PathBuf,
