80d917422f21ccc19417d295e66ee521d92152ed
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-08-22 02:54:52 -0700

    Fix compilation errors when missing impl

--- src/codegen/partials/std.c
+++ src/codegen/partials/std.c
@@ -1,6 +1,8 @@
+#include <assert.h>
 #include <inttypes.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
