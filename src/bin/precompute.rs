06465aba51df7322118bae5a905d384fe8504704
Sam Kaufman <emrysk@gmail.com>
2023-10-22 07:38:07 -0700

    Minor refactor.

--- src/bin/precompute.rs
+++ src/bin/precompute.rs
@@ -47,59 +47,7 @@ fn main() {
             .next_power_of_two(),
     );
     main_per_db(&args, &db);
-
-    let mut matmul_group_cnt = 0;
-    let mut matmul_entry_cnt = 0;
-    let mut conv_group_cnt = 0;
-    let mut conv_entry_cnt = 0;
-    let mut move_group_cnt = 0;
-    let mut move_entry_cnt = 0;
-    let mut zero_group_cnt = 0;
-    let mut zero_entry_cnt = 0;
-    for iref in db.blocks.iter() {
-        match iref.key().0 .0 {
-            SpecKey::Matmul { .. } => {
-                matmul_group_cnt += 1;
-                matmul_entry_cnt += iref.value().len();
-            }
-            SpecKey::Conv { .. } => {
-                conv_group_cnt += 1;
-                conv_entry_cnt += iref.value().len();
-            }
-            SpecKey::Move { .. } => {
-                move_group_cnt += 1;
-                move_entry_cnt += iref.value().len();
-            }
-            SpecKey::Zero { .. } => {
-                zero_group_cnt += 1;
-                zero_entry_cnt += iref.value().len();
-            }
-        }
-    }
-    println!(
-        "Matmul ratio: {}/{} = {:.2}",
-        matmul_group_cnt,
-        matmul_entry_cnt,
-        matmul_entry_cnt as f64 / matmul_group_cnt as f64
-    );
-    println!(
-        "Conv ratio: {}/{} = {:.2}",
-        conv_group_cnt,
-        conv_entry_cnt,
-        conv_entry_cnt as f64 / conv_group_cnt as f64
-    );
-    println!(
-        "Move ratio: {}/{} = {:.2}",
-        move_group_cnt,
-        move_entry_cnt,
-        move_entry_cnt as f64 / move_group_cnt as f64
-    );
-    println!(
-        "Zero ratio: {}/{} = {:.2}",
-        zero_group_cnt,
-        zero_entry_cnt,
-        zero_entry_cnt as f64 / zero_group_cnt as f64
-    );
+    print_stats(&db);
 }
 
 fn main_per_db<'a, D>(args: &Args, db: &'a D)
@@ -223,6 +171,61 @@ where
             break;
         }
     }
+}
+
+fn print_stats(db: &DashmapDiskDatabase) {
+    let mut matmul_group_cnt = 0;
+    let mut matmul_entry_cnt = 0;
+    let mut conv_group_cnt = 0;
+    let mut conv_entry_cnt = 0;
+    let mut move_group_cnt = 0;
+    let mut move_entry_cnt = 0;
+    let mut zero_group_cnt = 0;
+    let mut zero_entry_cnt = 0;
+    for iref in db.blocks.iter() {
+        match iref.key().0 .0 {
+            SpecKey::Matmul { .. } => {
+                matmul_group_cnt += 1;
+                matmul_entry_cnt += iref.value().len();
+            }
+            SpecKey::Conv { .. } => {
+                conv_group_cnt += 1;
+                conv_entry_cnt += iref.value().len();
+            }
+            SpecKey::Move { .. } => {
+                move_group_cnt += 1;
+                move_entry_cnt += iref.value().len();
+            }
+            SpecKey::Zero { .. } => {
+                zero_group_cnt += 1;
+                zero_entry_cnt += iref.value().len();
+            }
+        }
+    }
+    println!(
+        "Matmul ratio: {}/{} = {:.2}",
+        matmul_group_cnt,
+        matmul_entry_cnt,
+        matmul_entry_cnt as f64 / matmul_group_cnt as f64
+    );
+    println!(
+        "Conv ratio: {}/{} = {:.2}",
+        conv_group_cnt,
+        conv_entry_cnt,
+        conv_entry_cnt as f64 / conv_group_cnt as f64
+    );
+    println!(
+        "Move ratio: {}/{} = {:.2}",
+        move_group_cnt,
+        move_entry_cnt,
+        move_entry_cnt as f64 / move_group_cnt as f64
+    );
+    println!(
+        "Zero ratio: {}/{} = {:.2}",
+        zero_group_cnt,
+        zero_entry_cnt,
+        zero_entry_cnt as f64 / zero_group_cnt as f64
+    );
 }
 
 /// Returns a logical Move Spec of given size and rank.
