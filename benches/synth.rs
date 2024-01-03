da0aa479eb39cd2beced46ca76f6de7e561db5c5
Sam Kaufman <emrysk@gmail.com>
2024-01-02 22:09:40 -0800

    Add 'nonzero' crate.
    Instead of `SINGLE_JOB`, use a more general solution to the unwieldiness of constructing non-zero integer types.

--- benches/synth.rs
+++ benches/synth.rs
@@ -1,10 +1,10 @@
 use iai_callgrind::{black_box, main};
 use morello::db::DashmapDiskDatabase;
+use nonzero::nonzero as nz;
 use smallvec::smallvec;
 
 use morello::common::{DimSize, Dtype};
 use morello::layout::row_major;
-use morello::search::SINGLE_JOB;
 use morello::spec::{LogicalSpec, PrimitiveBasics, PrimitiveSpecType, Spec};
 use morello::target::{Target, X86Target};
 use morello::tensorspec::TensorSpecAux;
@@ -35,7 +35,7 @@ fn matmul_spec<Tgt: Target>(size: DimSize) -> Spec<Tgt> {
 
 fn synth(goal: &Spec<X86Target>) {
     let db = DashmapDiskDatabase::new(None, true, 1);
-    morello::search::top_down(&db, black_box(goal), 1, SINGLE_JOB);
+    morello::search::top_down(&db, black_box(goal), 1, Some(nz!(1usize)));
 }
 
 #[inline(never)]
--- src/bin/precompute.rs
+++ src/bin/precompute.rs
@@ -3,6 +3,7 @@ use tikv_jemallocator::Jemalloc;
 
 use clap::Parser;
 use log::{debug, info};
+use nonzero::nonzero as nz;
 use rand::seq::SliceRandom;
 use rayon::prelude::*;
 use smallvec::{smallvec, SmallVec};
@@ -15,7 +16,6 @@ use morello::datadeps::SpecKey;
 use morello::db::{DashmapDiskDatabase, Database};
 use morello::grid::general::SurMap;
 use morello::memorylimits::{MemVec, MemoryLimits};
-use morello::search::SINGLE_JOB;
 use morello::spec::{
     LogicalSpec, LogicalSpecSurMap, PrimitiveBasics, PrimitiveBasicsBimap, PrimitiveSpecType, Spec,
 };
@@ -170,7 +170,7 @@ where
                 worklist.push_back(top.clone());
                 while let Some(job) = worklist.pop_front() {
                     let spec = Spec(logical_spec.clone(), MemoryLimits::Standard(job));
-                    let result = morello::search::top_down(db, &spec, 1, SINGLE_JOB);
+                    let result = morello::search::top_down(db, &spec, 1, Some(nz!(1usize)));
                     if let [(_, only_result_cost)] = &result.0[..] {
                         worklist.extend(next_limits(&spec.1, &only_result_cost.peaks));
                     }
