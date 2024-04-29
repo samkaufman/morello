921dbf1c3e3fb3c169022e0b7999efdef470b741
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-04-29 16:58:44 -0700

    Removed unused import.

--- morello/benches/synth.rs
+++ morello/benches/synth.rs
@@ -1,7 +1,6 @@
 use iai_callgrind::{black_box, main};
 use nonzero::nonzero as nz;
 
-use morello::common::DimSize;
 use morello::db::RocksDatabase;
 use morello::layout::row_major;
 use morello::lspec;
