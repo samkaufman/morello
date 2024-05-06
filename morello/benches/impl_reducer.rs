37218cf6882308c6ea0af1a1e1c2f1e1e1723bb1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-06 02:19:31 -0700

    Generated random costs.

--- morello/benches/impl_reducer.rs
+++ morello/benches/impl_reducer.rs
@@ -1,37 +1,43 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+use rand::prelude::*;
 use smallvec::smallvec;
 
 use morello::cost::Cost;
 use morello::db::ActionIdx;
 use morello::memorylimits::MemVec;
 use morello::search::ImplReducer;
-use morello::target::X86Target;
 
 #[inline(never)]
-fn impl_reducer(top_k: usize, action_indices: &[ActionIdx]) {
+fn impl_reducer(top_k: usize, actions: &[(ActionIdx, Cost)]) {
     let mut reducer = ImplReducer::new(top_k, smallvec![]);
-    action_indices.iter().for_each(|&action_idx| {
-        reducer.insert(
-            action_idx,
-            Cost {
-                main: 1,
-                peaks: MemVec::zero::<X86Target>(),
-                depth: 0,
-            },
-        )
-    });
+    actions
+        .iter()
+        .for_each(|(action_idx, cost)| reducer.insert(*action_idx, cost.clone()));
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("ImplReducer");
     for top_k in [1, 2, 8, 100] {
-        let mut action_indices: Vec<ActionIdx> = (0..top_k + 10000).collect();
-        action_indices.shuffle(&mut thread_rng());
+        let mut rng = rand::thread_rng();
+        let mut actions: Vec<ActionIdx> = (0..top_k + 10000).collect();
+        actions.shuffle(&mut rng);
+        let actions: Vec<_> = actions
+            .into_iter()
+            .map(|action_idx| {
+                (
+                    action_idx,
+                    Cost {
+                        main: rng.gen(),
+                        peaks: MemVec::new([rng.gen(), rng.gen(), rng.gen(), rng.gen()]),
+                        depth: rng.gen(),
+                    },
+                )
+            })
+            .collect();
+
         group.bench_with_input(
             BenchmarkId::new("impl_reducer", top_k),
-            &(usize::from(top_k), &action_indices[..]),
+            &(usize::from(top_k), &actions[..]),
             |b, (k, actions)| b.iter(|| impl_reducer(black_box(*k), black_box(actions))),
         );
     }
