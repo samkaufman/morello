42b19042f218e20a864cb8bb675c6783823e3f9e
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-06 09:24:07 -0700

    Added tango, removed criterion.

--- morello/benches/impl_reducer.rs
+++ morello/benches/impl_reducer.rs
@@ -1,7 +1,8 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use rand::prelude::*;
 use rand_chacha::ChaCha8Rng;
 use smallvec::smallvec;
+use std::hint::black_box;
+use tango_bench::{benchmark_fn, tango_benchmarks, tango_main, IntoBenchmarks};
 
 use morello::cost::Cost;
 use morello::db::ActionIdx;
@@ -9,42 +10,37 @@ use morello::memorylimits::MemVec;
 use morello::search::ImplReducer;
 
 #[inline(never)]
-fn impl_reducer(top_k: usize, actions: &[(ActionIdx, Cost)]) {
-    let mut reducer = ImplReducer::new(top_k, smallvec![]);
+fn impl_reducer(top_k: u16) {
+    let mut rng = ChaCha8Rng::seed_from_u64(2);
+    let mut reducer = ImplReducer::new(usize::from(top_k), smallvec![]);
+    let mut actions: Vec<ActionIdx> = (0..top_k + 10000).collect();
+    actions.shuffle(&mut rng);
     actions
-        .iter()
-        .for_each(|(action_idx, cost)| reducer.insert(*action_idx, cost.clone()));
+        .into_iter()
+        .map(|action_idx| {
+            (
+                action_idx,
+                Cost {
+                    main: rng.gen(),
+                    peaks: MemVec::new([rng.gen(), rng.gen(), rng.gen(), rng.gen()]),
+                    depth: rng.gen(),
+                },
+            )
+        })
+        .for_each(|(action_idx, cost)| reducer.insert(action_idx, cost));
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("ImplReducer");
-    for top_k in [1, 2, 8, 100] {
-        let mut rng = ChaCha8Rng::seed_from_u64(2);
-        let mut actions: Vec<ActionIdx> = (0..top_k + 10000).collect();
-        actions.shuffle(&mut rng);
-        let actions: Vec<_> = actions
-            .into_iter()
-            .map(|action_idx| {
-                (
-                    action_idx,
-                    Cost {
-                        main: rng.gen(),
-                        peaks: MemVec::new([rng.gen(), rng.gen(), rng.gen(), rng.gen()]),
-                        depth: rng.gen(),
-                    },
-                )
-            })
-            .collect();
-
-        group.bench_with_input(
-            BenchmarkId::new("impl_reducer", top_k),
-            &(usize::from(top_k), &actions[..]),
-            |b, (k, actions)| b.iter(|| impl_reducer(black_box(*k), black_box(actions))),
-        );
-    }
-    group.finish();
+fn impl_reducer_benchmarks() -> impl IntoBenchmarks {
+    [
+        benchmark_fn("impl_reducer: 1", |b| b.iter(|| impl_reducer(black_box(1)))),
+        benchmark_fn("impl_reducer: 2", |b| b.iter(|| impl_reducer(black_box(2)))),
+        benchmark_fn("impl_reducer: 8", |b| b.iter(|| impl_reducer(black_box(8)))),
+        benchmark_fn("impl_reducer: 100", |b| {
+            b.iter(|| impl_reducer(black_box(100)))
+        }),
+    ]
 }
 
 // TODO: Convert benchmark to use iai_callgrind.
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+tango_benchmarks!(impl_reducer_benchmarks());
+tango_main!();
