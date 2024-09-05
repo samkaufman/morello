90f4c93c7be1f55965f55222f3de0c5da981dd79
Sam Kaufman <emrysk@gmail.com>
2024-09-05 12:19:53 -0700

    fix: x86 flags now include -mfma

--- morello/src/codegen/mod.rs
+++ morello/src/codegen/mod.rs
@@ -31,7 +31,7 @@ pub use self::cpu::CpuCodeGenThreadStyle;
 const CLI_FLAGS: [&str; 4] = ["-std=gnu99", "-O3", "-rtlib=compiler-rt", "-o"];
 
 // TODO: Avoid -fopenmp if we're not using an OpenMP pool.
-const X86_CLI_VEC_FLAGS: [&str; 2] = ["-fopenmp", "-mavx2"];
+const X86_CLI_VEC_FLAGS: [&str; 3] = ["-fopenmp", "-mavx2", "-mfma"];
 const ARM_CLI_VEC_FLAGS: [&str; 1] = ["-fopenmp"];
 
 const MIN_SAMPLES: u32 = 3;
