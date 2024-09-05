7b30ba91919958703c485238d96ccbd7f727293d
Sam Kaufman <emrysk@gmail.com>
2024-09-05 12:49:06 -0700

    fix matvec_gemma_x86_bf16f32 for new Move sub-Spec logic.

--- morello/examples/matvec_gemma_x86_bf16f32.rs
+++ morello/examples/matvec_gemma_x86_bf16f32.rs
@@ -71,11 +71,7 @@ fn main() {
                     body.move_param(1, CpuMemoryLevel::L1, col_major(2), None)
                         .place(CpuKernel::DotProductLoopF32InterleavedBf16F32)
                 })
-                .subschedule(&[1], |body| {
-                    body.move_param(0, CpuMemoryLevel::L1, col_major(2), None)
-                        .subschedule(&[0], |z| z.place(CpuKernel::ValueAssign))
-                        .subschedule(&[1], |z| z.force_place(CpuKernel::ValueAssign))
-                })
+                .subschedule(&[1], |body| body.place(CpuKernel::ValueAssign))
         });
 
     implementation
