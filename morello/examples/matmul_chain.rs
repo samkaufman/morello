8b653fcff48071bafcba218ff62d176f1ae6c7bc
Sam Kaufman <emrysk@gmail.com>
2024-10-28 13:00:02 -0700

    matmul_chain: faster.

--- morello/examples/matmul_chain.rs
+++ morello/examples/matmul_chain.rs
@@ -1,7 +1,6 @@
 use morello::codegen::CodeGen;
 use morello::common::Dtype;
 use morello::cost::Cost;
-use morello::db::FilesDatabase;
 use morello::imp::ImplNode;
 use morello::layout::{row_major, Layout, PhysDim};
 use morello::pprint::ImplPrintStyle;
@@ -17,19 +16,18 @@ use morello::tensorspec::TensorSpecAux;
 use morello::utils::ToWriteFmt;
 
 use std::io;
-use std::path::Path;
 
 use nonzero::nonzero as nz;
 
 fn main() {
     let basics0 = PrimitiveBasics {
         typ: PrimitiveSpecType::Matmul { accum: false },
-        spec_shape: shape![1024, 1024, 1024],
+        spec_shape: shape![2048, 2048, 2048],
         dtypes: vec![Dtype::Float32, Dtype::Float32, Dtype::Float32],
     };
     let basics1 = PrimitiveBasics {
         typ: PrimitiveSpecType::Matmul { accum: false },
-        spec_shape: shape![1024, 1024, 1024],
+        spec_shape: shape![2048, 2048, 2048],
         dtypes: vec![Dtype::Float32, Dtype::Float32, Dtype::Float32],
     };
     let aux = TensorSpecAux {
@@ -50,19 +48,18 @@ fn main() {
     );
     spec.canonicalize().unwrap();
 
-    let db = FilesDatabase::new(
-        Some(Path::new("./matmul_chain.db")),
-        true,
-        1,
-        2usize.pow(22),
-        2,
-        None,
-    );
     let imp = spec
-        .tile_out(&[128, 128])
         .bufferize(0, GL, row_major(2), None)
-        .subschedule(&[0], |s| schedule_matmul(&db, s))
-        .subschedule(&[1], |s| schedule_matmul(&db, s));
+        .subschedule(&[0], |s| {
+            s.to_accum()
+                .subschedule(&[0], schedule_zero)
+                .subschedule(&[1], schedule_matmulaccum)
+        })
+        .subschedule(&[1], |s| {
+            s.to_accum()
+                .subschedule(&[0], schedule_zero)
+                .subschedule(&[1], schedule_matmulaccum)
+        });
     imp.emit(
         false,
         Some(ImplPrintStyle::Compact),
@@ -87,7 +84,7 @@ fn main() {
     }
 
     // Benchmark.
-    const ITERS: u32 = 5;
+    const ITERS: u32 = 10;
     let result = imp
         .bench(ITERS, None)
         .unwrap_or_else(|e| panic!("Failed to benchmark: {}", e));
@@ -99,20 +96,10 @@ fn main() {
     println!("// kernel runtime: {kernel_runtime:.4}s ({throughput:.2}/sec)",);
 }
 
-fn schedule_matmul(db: &FilesDatabase, spec: &Spec<X86Target>) -> ImplNode<X86Target> {
-    let mat1_pack_size = nz!(16u32);
-    let layout_b = Layout::new(vec![
-        (1, PhysDim::Dynamic),
-        (0, PhysDim::Dynamic),
-        (1, PhysDim::Packed(mat1_pack_size)),
-    ]);
-    spec.to_accum()
-        .subschedule(&[0], |zero_spec| {
-            zero_spec.tile_out(&[1, 4]).synthesize(db, None)
-        })
-        .split(128)
-        .move_param(1, GL, layout_b.clone(), None)
-        .subschedule(&[1, 0], |pack_b| {
+fn schedule_matmulaccum(spec: &Spec<X86Target>) -> ImplNode<X86Target> {
+    spec.split(128)
+        .move_param(1, GL, layout_b(), None)
+        .subschedule(&[0], |pack_b| {
             // TODO: This stinks. Use vectors at least.
             pack_b
                 .tile_out(&[1, 1])
@@ -122,20 +109,39 @@ fn schedule_matmul(db: &FilesDatabase, spec: &Spec<X86Target>) -> ImplNode<X86Ta
                 .subschedule(&[0], |m0| m0.place(CpuKernel::ValueAssign))
                 .subschedule(&[1], |m0| m0.place(CpuKernel::ValueAssign))
         })
+        .tile_out(&[128, 1024])
         .tile_out(&[4, 16])
         .move_param(0, L1, row_major(2), None)
-        .move_param(1, L1, layout_b.clone(), None)
+        .move_param(1, L1, layout_b(), None)
         .move_param(2, L1, row_major(2), None)
         .move_param(2, VRF, row_major(2), Some(nz!(8u32)))
-        .subschedule(&[1, 1, 0], |m| {
+        .subschedule(&[1, 0], |m| {
             m.tile_out(&[1, 8]).place(CpuKernel::VectorAssign)
         })
-        .subschedule(&[1, 1, 2], |m| {
+        .subschedule(&[1, 2], |m| {
             m.tile_out(&[1, 8]).place(CpuKernel::VectorAssign)
         })
         .split(1)
         .tile_out(&[1, 16])
-        .move_param(1, VRF, layout_b.clone(), Some(nz!(8u32)))
-        .subschedule(&[1, 1, 1, 0], |m| m.place(CpuKernel::VectorAssign))
-        .subschedule(&[1, 1, 1, 1], |m| m.place(CpuKernel::BroadcastVecMultAdd))
+        .move_param(1, VRF, layout_b(), Some(nz!(8u32)))
+        .subschedule(&[1, 1, 0], |m| m.place(CpuKernel::VectorAssign))
+        .subschedule(&[1, 1, 1], |m| m.place(CpuKernel::BroadcastVecMultAdd))
+}
+
+fn schedule_zero(spec: &Spec<X86Target>) -> ImplNode<X86Target> {
+    spec.tile_out(&[32, 1])
+        .move_param(0, L1, row_major(2), None)
+        .tile_out(&[16, 1])
+        .move_param(0, RF, row_major(2), None)
+        .subschedule(&[0], |s| s.tile_out(&[1, 1]).place(CpuKernel::MemsetZero))
+        .subschedule(&[1], |s| s.tile_out(&[1, 1]).place(CpuKernel::ValueAssign))
+}
+
+fn layout_b() -> Layout {
+    let mat1_pack_size = nz!(16u32);
+    Layout::new(vec![
+        (1, PhysDim::Dynamic),
+        (0, PhysDim::Dynamic),
+        (1, PhysDim::Packed(mat1_pack_size)),
+    ])
 }
