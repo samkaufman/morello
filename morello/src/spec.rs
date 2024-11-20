45c5327bea02077672e611b6a6e87ff2928b67e8
Sam Kaufman <emrysk@gmail.com>
2024-11-20 08:03:30 -0800

    #[ignore] a few more tests

--- morello/src/spec.rs
+++ morello/src/spec.rs
@@ -2327,6 +2327,7 @@ mod tests {
         }
 
         #[test]
+        #[ignore]
         fn test_actions_are_valid_through_consumed_memory_x86(
             logical_spec in arb_canonical_logical_spec::<X86Target>(Some(TEST_SMALL_SIZE))
         ) {
@@ -2334,6 +2335,7 @@ mod tests {
         }
 
         #[test]
+        #[ignore]
         fn test_actions_are_valid_through_consumed_memory_arm(
             logical_spec in arb_canonical_logical_spec::<X86Target>(Some(TEST_SMALL_SIZE))
         ) {
